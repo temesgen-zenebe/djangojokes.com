e380cb30a9b9131aaa5fa5aecedb07e222364433
Temesgen zenebe <temf2006@gmail.com>
2022-05-06 00:12:16 -0700

    Creating a DetailView-create the page to show information about an individual joke

--- jokes/models.py
+++ jokes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Joke(models.Model):
@@ -7,5 +8,10 @@ class Joke(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    def get_absolute_url(self):
+        return reverse('jokes:detail', args=[str(self.pk)])
+
     def __str__(self):
         return self.question
+
+        
