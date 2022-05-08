6830cf0e1cfde9bb61889608b4fb94854360dba1
Temesgen zenebe <temf2006@gmail.com>
2022-05-07 20:50:00 -0700

    Creating and Updating Jokes

--- jokes/views.py
+++ jokes/views.py
@@ -1,8 +1,16 @@
-from django.views.generic import DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from .models import Joke
+
+class JokeCreateView(CreateView):
+    model = Joke
+    fields = ['question', 'answer']
 
 class JokeDetailView(DetailView):
     model = Joke
 
 class JokeListView(ListView):
     model = Joke
+
+class JokeUpdateView(UpdateView):
+    model = Joke
+    fields = ['question', 'answer']
