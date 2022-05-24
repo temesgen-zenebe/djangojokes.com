b23cb7ffc60ac9255b5595fe040111782b034128
Temesgen zenebe <temf2006@gmail.com>
2022-05-24 04:46:18 -0700

    Adding Messages and CSS Rules for the Message Classes

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django.contrib import messages
 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
@@ -6,3 +7,11 @@ class HomePageView(TemplateView):
 
 class AboutUsView(TemplateView):
     template_name = 'pages/about_us.html'
+
+    def get(self, request, *args, **kwargs):
+        messages.debug(request, 'Debug message.')
+        messages.info(request, 'Info message.')
+        messages.success(request, 'Success message.')
+        messages.warning(request, 'Warning message.')
+        messages.error(request, 'Error message.')
+        return super().get(request, args, kwargs)
